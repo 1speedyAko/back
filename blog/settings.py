41c7b42158ef0d54f158c2641e8170434c993b16
charlie <charloako1@gmail.com>
2024-08-17 15:56:50 +0300

    added allowed hosts

--- blog/settings.py
+++ blog/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition

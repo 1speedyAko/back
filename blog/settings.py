6bfb7524cc72958628ccc6407c42413afa8ce25c
charlie <charloako1@gmail.com>
2024-08-17 16:57:27 +0300

    added veracel to cors

--- blog/settings.py
+++ blog/settings.py
@@ -120,6 +120,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://predictoriouszone.vercel.app/",
 ]
 
 ROOT_URLCONF = 'blog.urls'

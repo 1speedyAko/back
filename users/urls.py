03b6c47504783b73f1574d7df5fc10fc8c7d0f1b
charlie <charloako1@gmail.com>
2024-09-10 18:29:08 +0300

    removed user detail view

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
-from .views import LogoutView, UserDetailView
+from .views import LogoutView
 
 urlpatterns = [
-    path('me/', UserDetailView.as_view(), name='user-detail'),
+    path("auth/logout/", LogoutView.as_view(), name="logout"),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
-    path("auth/logout/", LogoutView.as_view(), name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -3,15 +3,9 @@ from rest_framework import generics,permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .serializers import  CustomUserCreateSerializer
 
 
-class UserDetailView(generics.RetrieveAPIView):
-    serializer_class = CustomUserCreateSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
-    def get_object(self):
-        return self.request.user
 
 class LogoutView(APIView):
     permission_classes = (IsAuthenticated,)

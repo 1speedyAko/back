171ec12e80b14fa7ac7f7478aa6e114e7e06c153
charlie <charloako1@gmail.com>
2024-09-11 15:58:21 +0300

    jwt only

--- blog/settings.py
+++ blog/settings.py
@@ -268,11 +268,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',  # For token-based authentication
-        'rest_framework.authentication.SessionAuthentication',  # For session-based authentication
-        'rest_framework.authentication.BasicAuthentication',  # For basic authentication
+        
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+     
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
